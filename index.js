47585652458a9ab062b5b4355d4e85dac86a8255
Rafael Alves <hrafael.alves9@gmail.com>
2022-04-06 19:25:40 -0300

    up show word

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //variáveis frequentimente usadas
 const select = e => document.querySelector(e);
-
+//lista de palavras
 var wordList = [
     'sagaz', 'negro', 'âmago', 'êxito', 'mexer', 'termo', 'senso', 'nobre', 'algoz', 'afeto', 'plena', 'ética', 'mútua', 'sutil', 'tênue', 'vigor', 'aquém', 'audaz', 'porém', 'fazer', 'sanar', 'seção', 'inato', 'assim', 'cerne', 'ideia', 'desde', 'fosse', 'poder', 'moral', 'torpe', 'honra', 'muito', 'justo', 'gozar', 'anexo', 'fútil', 'razão', 'quiçá', 'etnia', 'ícone', 'sobre', 'tange', 'égide', 'lapso', 'mútuo', 'sonho', 'expor', 'haver', 'hábil', 'casal', 'amigo', 'tempo', 'pesar', 'posse', 'ávido', 'então', 'boçal', 'ardil', 'coser', 'genro', 'corja', 'seara', 'dengo', 'prole', 'detém', 'causa', 'dizer', 'tenaz', 'digno', 'porta', 'dever', 'crivo', 'saber', 'óbice', 'ápice', 'ânsia', 'brado', 'ceder', 'ânimo', 'gleba', 'pária', 'graça', 'assaz', 'comum', 'atroz', 'orgia', 'culto', 'temor', 'sendo', 'mundo', 'censo', 'pauta', 'fugaz', 'denso', 'cozer', 'valha', 'neném', 'ainda', 'vício'
 ];
@@ -14,18 +14,19 @@ function randomNumber(min, max) {
 const numberSelected = randomNumber(0,99);
 const wordSelected = wordList[numberSelected]; //palavra da vez
 
-//fileiras de tentativas
-const fileira1 = select('#tentativa1');
+//referência de cada letra da palavra sorteada
+const letra0 = wordSelected.substring(0,1).toUpperCase();
+const letra1 = wordSelected.substring(1,2).toUpperCase();
+const letra2 = wordSelected.substring(2,3).toUpperCase();
+const letra3 = wordSelected.substring(3,4).toUpperCase();
+const letra4 = wordSelected.substring(4,5).toUpperCase();
 
-const letra1 = wordSelected.substring(0,1).toUpperCase();
-const letra2 = wordSelected.substring(1,2).toUpperCase();
-const letra3 = wordSelected.substring(2,3).toUpperCase();
-const letra4 = wordSelected.substring(3,4).toUpperCase();
-const letra5 = wordSelected.substring(4,5).toUpperCase();
-
-//fileira de tentativa atual
+//fileiras da tentativas atual
 const atual = select('.atual')
 
-// if(atual.children[0].innerHTML == null){
-//     atual.children[0].innerHTML = e
-// }
+//mostrando na tela (teste)
+atual.children[0].innerHTML = letra0
+atual.children[1].innerHTML = letra1
+atual.children[2].innerHTML = letra2
+atual.children[3].innerHTML = letra3
+atual.children[4].innerHTML = letra4
